dc755a0a8e5eca81e31350fdde68eceed61b47e2
Ibrahim Farajzade <ibrahimfarajzade@gmail.com>
2025-03-23 00:27:46 +0200

    Create header and body, implement Movie DB API

---
+++ src/components/Search.tsx
@@ -0,0 +1,23 @@
+interface SearchProps {
+  searchTerm: string
+  setSearchTerm: (term: string) => void
+}
+
+const Search = ({ searchTerm, setSearchTerm }: SearchProps) => {
+  return (
+    <div className='search'>
+      <div>
+        <img src='search.svg' alt='search' />
+
+        <input
+          type='text'
+          placeholder='Search for a movie...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Search
